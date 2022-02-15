41ad1565eabd40a2b91be2d8144999f1ced0f04b
Kevin Ordener <k.ordener.dev@gmail.com>
2022-02-15 01:21:05 -0600

    tip-calculator

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './js-practice-projects/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
